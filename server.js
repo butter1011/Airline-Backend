c8a5c6cc874f683e31acbc30b7d29c985dce0389
topdeveloper1219 <kanjirohonda@gmail.com>
2024-11-14 00:16:33 -0500

    feat:add cors function

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require('body-parser');
+const cors = require("cors");
 
 const app = express();
 const connectDB = require("./utils/connectDB.js");
@@ -10,6 +11,7 @@ const PORT = process.env.PORT || 3000;
 // Connect to MongoDB
 connectDB();
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
