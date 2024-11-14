4b2170e7def5da99f2c025270023fa316583fa35
butter1011 <luckybutter322982@gmail.com>
2024-11-14 19:25:46 +1000

    fix: fix the api for review

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -13,10 +13,14 @@ const {
 
 /// Post api
 router.post("/api/v1/user", createUserInfo);
-router.post("/api/v1/airline-airport/create", createAirlineAirport);
-router.post("/api/v1/airport-review", createAirportReview);
-router.post("/api/v1/airline-review", createAirlineReview);
+router.post("/api/v1/airline-airport/review", createAirlineReview);
 
+/// Unused API
+router.post("/api/v1/airport-review", createAirportReview);
+
+// Postman API
+router.post("/api/v1/airline-airport/create", createAirlineAirport);
 router.post("/api/v1/airline-airport/update", updateAirlineAirport);
 
 module.exports = router;
+
