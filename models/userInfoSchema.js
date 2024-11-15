a75f2dfd2fc942ee4434d77a029c05269f8e9de4
Pioneer220 <morrishart0220@gmail.com>
2024-11-14 22:54:17 -0800

    edtingUserdata

--- models/userInfoSchema.js
+++ models/userInfoSchema.js
@@ -5,46 +5,61 @@ const userInfoSchema = new mongoose.Schema(
     name: {
       type: String,
       required: true,
+      unique: false,
     },
     email: {
       type: String,
       required: false,
-      unique: false,
+      unique: true,
+    },
+    flyertype: {
+      type: String,
+      required: false,
+      default: "Business Class",
+    },
+    topReviewer: {
+      type: String,
+      required: false,
+      default: "TopReviewer",
     },
     whatsappNumber: {
       type: String,
       required: false,
-      unique: false,
     },
     points: {
       type: Number,
       required: false,
-      default: 0,
+      default: +500,
     },
     badges: {
       type: Number,
       required: false,
-      default: 0,
+      default: +9,
     },
     travelHistory: {
       type: Object,
       required: false,
+      unique: false,
     },
     preferences: {
       type: Object,
       required: false,
+      unique: false,
     },
     profilePhoto: {
       type: String,
       required: false,
+      unique: false,
     },
     bio: {
       type: String,
       required: false,
+      default: "I am TopReviewer",
     },
     favoriteAirlines: {
       type: String,
       required: false,
+      default: "British Airways",
     },
     language: {
       type: String,
