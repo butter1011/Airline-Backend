e112b7346806d5ef7d71b68f69a1aecba2c937ae
topdeveloper1219 <kanjirohonda@gmail.com>
2024-11-28 15:56:24 +0900

    feat: add getAirlineScore controller

--- routes/getRoutes.js
+++ routes/getRoutes.js
@@ -5,10 +5,13 @@ const {
 } = require("../controllers/airportAirlineController");
 const { getAirlineReviews } = require("../controllers/airlineReviewController");
 const { getBoardingPass } = require("../controllers/boardingPassController");
+const { getAirlineScore } = require("../controllers/airlineScoreController");
 
 // Get api
 router.get("/api/v2/airline-airport", getAirlineAirport);
 router.get("/api/v2/airline-reviews", getAirlineReviews);
 router.get("/api/v2/boarding-pass", getBoardingPass);
+router.get("/api/v2/airline-score", getAirlineScore);
+
 
 module.exports = router;
