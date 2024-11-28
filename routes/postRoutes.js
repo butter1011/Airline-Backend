03d52ca1ff777e7ef052a5a898007049711415b2
butter1011 <luckybutter322982@gmail.com>
2024-11-29 02:20:09 +1000

    fix: add the rating feature
    Co-authored-by: Pioneer220 <morrishart0220@gmail.com>
    Co-authored-by: topdeveloper1219 <kanjirohonda@gmail.com>

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -27,9 +27,9 @@ router.post("/api/v1/editUser", editUserInfo);
 router.post("/api/v1/badgeEditUser", badgeEditUserInfo);
 router.post("/api/v1/airport-review", createAirportReview);
 router.post("/api/v1/airline-review", createAirlineReview);
+router.post("/api/v1/airline-review/update", updateAirlineReview);
 router.post("/api/v1/boarding-pass", createBoardingPass);
 router.post("/api/v1/boarding-pass/update", updateBoardingPass);
-router.post("/api/v1/airline-review/update", updateAirlineReview);
 
 // Postman API
 router.post("/api/v1/airline-airport/create", createAirlineAirport);
