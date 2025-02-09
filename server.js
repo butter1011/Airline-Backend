5d2fac9e61f3e95b1a180ad20a0fb0dd38480900
butter1011 <luckybutter322982@gmail.com>
2025-02-09 14:50:41 +0900

    fix: enhance CORS configuration for improved API access

--- server.js
+++ server.js
@@ -13,11 +13,14 @@ const postApi = require("./routes/postRoutes.js");
 // Connect to MongoDB
 connectDB();
 
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 
 // Routes
 app.use(postApi);
