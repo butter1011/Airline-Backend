6fed46485311caed9083e076c4659f70dea315a5
butter1011 <luckybutter322982@gmail.com>
2025-02-09 15:15:16 +0900

    fix: enhance CORS configuration with specific methods and headers

--- server.js
+++ server.js
@@ -17,7 +17,11 @@ connectDB();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(cors("*"));
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 
 // Routes
 app.use(postApi);
